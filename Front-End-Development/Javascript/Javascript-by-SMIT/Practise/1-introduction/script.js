77d00d06f6dde86d0d304b6d0c76deda0bc84db5
Omama Khokhar <workwithomama@outlook.com>
2024-08-25 04:42:03 +0500

    Learned Chapter 1 - Alerts (SMIT)

---
+++ Front-End-Development/Javascript/Javascript-by-SMIT/Practise/1-introduction/script.js
@@ -0,0 +1,5 @@
+// Chapter 1: Alert
+// Alert: An alert is a pop-up box that displays a message to the user.
+// Syntax: The alert keyword is followed by parentheses, with the message inside them. 
+
+alert("Asslam u Alikum");
