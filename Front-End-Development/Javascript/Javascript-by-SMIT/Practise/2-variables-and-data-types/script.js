25b5b103cde6545393c943fdac6bcee76cbc9603
Omama Khokhar <workwithomama@outlook.com>
2024-08-25 05:55:44 +0500

    Learned Chapter 2 -  Variables for Strings (SMIT)

---
+++ Front-End-Development/Javascript/Javascript-by-SMIT/Practise/2-variables-and-data-types/script.js
@@ -0,0 +1,11 @@
+// Variable:  A variable is like a container that stores information (a value) that can be used later in your code
+// Creating a Variable: To create a variable, you use the keyword var, followed by the variable's name and an equals sign (=) to assign it a value.
+
+// Chapter 2: Variables in String
+// A variable in string means it is a container that stores a value, such as a text string and the text must always be enclosed in quotes.
+// Concatenation (+): The + operator in JavaScript combines two or more strings into one continuous string.
+
+var fullName = "Omama Khokhar";
+fullName = "Noor ul Ain";
+
+alert("Hello, " + fullName + "!");
