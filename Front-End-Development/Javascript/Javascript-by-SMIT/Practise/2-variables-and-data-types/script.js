00a1a339f493883c4ebb1429549f2e5a584d58ff
Omama Khokhar <workwithomama@outlook.com>
2024-08-25 23:59:02 +0500

    Learned Chapter 3 - Variables for Numbers (SMIT)

--- Front-End-Development/Javascript/Javascript-by-SMIT/Practise/2-variables-and-data-types/script.js
+++ Front-End-Development/Javascript/Javascript-by-SMIT/Practise/2-variables-and-data-types/script.js
@@ -1,11 +1,23 @@
 // Variable:  A variable is like a container that stores information (a value) that can be used later in your code
 // Creating a Variable: To create a variable, you use the keyword var, followed by the variable's name and an equals sign (=) to assign it a value.
 
-// Chapter 2: Variables in String
-// A variable in string means it is a container that stores a value, such as a text string and the text must always be enclosed in quotes.
+// Slice: The slice(startIndex, endIndex) method is used to extract a section of a string or array and return it as a new string or array, without modifying the original string or array. 
 // Concatenation (+): The + operator in JavaScript combines two or more strings into one continuous string.
 
-var fullName = "Omama Khokhar";
-fullName = "Noor ul Ain";
+// Chapter 2: Variables in String
+// A variable in string means it is a container that stores a value, such as a text string and the text must always be enclosed in quotes.
+
+var fullName = "Umama Khokhar";
+fullName = "Omama Khokhar";
 
 alert("Hello, " + fullName + "!");
+
+// Chapter 3: Variables for Numbers
+// A variable in number means it is a container that stores a value, such as a number 
+var weigth = 55;
+var newWeight = 60;
+
+var gainWeight;
+gainWeight = newWeight - weigth;
+
+alert("Your Gain Weight is " + gainWeight);
