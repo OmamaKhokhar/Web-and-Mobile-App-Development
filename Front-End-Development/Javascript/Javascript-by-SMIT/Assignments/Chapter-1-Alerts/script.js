ec151a814614275e9649e7e11659d3f6812eb0d6
Omama Khokhar <workwithomama@outlook.com>
2024-08-25 05:07:54 +0500

    Add JavaScript Assignment for Chapter 1 - Alerts

---
+++ Front-End-Development/Javascript/Javascript-by-SMIT/Assignments/Chapter-1-Alerts/script.js
@@ -0,0 +1,14 @@
+// Chapter 1: Alerts
+
+// Exercise 1: Greet the website visitor using a JS alert box
+alert("Hello Friends!")
+
+// Exercise 2: Display a specific message on your web page
+alert("Error! Please enter a valid password.")
+
+// Exercise 3: Display a message on your web page with a line break
+alert("Welcome to JS Land...\nHappy Coding!")
+
+// Exercise 4: Display multiple messages in sequence using alerts
+alert("Welcome to JS Land...")
+alert("Happy Coding!")
