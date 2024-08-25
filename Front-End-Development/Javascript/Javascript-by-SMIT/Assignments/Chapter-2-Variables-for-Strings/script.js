98692209447e4a1d92a1871b5f3d61f37286e900
Omama Khokhar <workwithomama@outlook.com>
2024-08-25 23:57:55 +0500

    Add JavaScript Assignment for Chapter 2 - Variables for Strings

---
+++ Front-End-Development/Javascript/Javascript-by-SMIT/Assignments/Chapter-2-Variables-for-Strings/script.js
@@ -0,0 +1,54 @@
+// Chapter 2: Variables for Strings
+
+// 1. Declare a variable to store the username.
+var username;
+
+// 2. Declare a variable for your full name and assign it.
+var fullName = "Omama Khokhar";
+
+// 3. Declare a variable for the message, assign "Hello World," and display it.
+var message;
+message = "Hello World";
+alert(message);
+
+// 4. Store student's bio data in variables and show in alert boxes.
+var studentName = "Omama Khokahr";
+alert(studentName);
+
+var age = "17 year old";
+alert(age);
+
+var course = "Web and Mobile App Development";
+alert(course);
+
+// 5. Display a predefined message using a single variable.
+var food = "PIZZA";
+alert(
+  food +
+    "\n" +
+    food.slice(0, 4) +
+    "\n" +
+    food.slice(0, 3) +
+    "\n" +
+    food.slice(0, 2) +
+    "\n" +
+    food.slice(0, 1)
+);
+
+// 6. Declare a variable for email and show it in an alert with a message.
+var email = "workwithomama@gmail.com";
+alert("My email address is " + email);
+
+// 7. Declare a variable for the book title and display it in an alert.
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// 8. Display a message directly in the browser using JavaScript.
+var message = "<h1>Yah! I can write HTML content through JavaScript</h1>";
+
+// document.write() is a JavaScript method to output HTML or text directly into the web page at the script's execution point.
+document.write(message);
+
+// 9. Store a special string in a variable and show it in an alert and the browser.
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
