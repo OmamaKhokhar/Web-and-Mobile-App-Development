6880311a1386aa42932085c5b1eeef0a3fb8af5e
Omama Khokhar <workwithomama@outlook.com>
2024-08-29 01:55:32 +0500

    Learned Chapter 5 - Math Expressions: familiar operators (SMIT)

---
+++ Front-End-Development/Javascript/Javascript-by-SMIT/Practise/3-mathematical-operations-and-managing-numbers/script.js
@@ -0,0 +1,14 @@
+// Chapter 5: Math Expressions: familiar operators
+// Basic math in JavaScript includes addition (+), subtraction (-), multiplication (*), division (/) and Modulus Operator %.
+// Variables can be used in expressions, either combined with numbers or with other variables.
+var subjectCount = 5;
+var marksPerSubject = 100;
+
+var math = 70;
+var english = 80;
+var science = 90;
+var urdu = 75;
+var socialScience = 85;
+
+var totalMarks = marksPerSubject * subjectCount;
+var obtainedMarks = math + english + science + urdu + socialScience;
