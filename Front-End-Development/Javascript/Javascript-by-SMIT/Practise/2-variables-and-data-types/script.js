c6c21a3dfc1d4f5f3088428a11fc72e13f31a06f
Omama Khokhar <workwithomama@outlook.com>
2024-08-30 02:35:11 +0500

    Learned Chapter 8 - Concatenating text strings (SMIT)

--- Front-End-Development/Javascript/Javascript-by-SMIT/Practise/2-variables-and-data-types/script.js
+++ Front-End-Development/Javascript/Javascript-by-SMIT/Practise/2-variables-and-data-types/script.js
@@ -2,7 +2,6 @@
 // Creating a Variable: To create a variable, you use the keyword var, followed by the variable's name and an equals sign (=) to assign it a value.
 
 // Slice: The slice(startIndex, endIndex) method is used to extract a section of a string or array and return it as a new string or array, without modifying the original string or array. 
-// Concatenation (+): The + operator in JavaScript combines two or more strings into one continuous string.
 
 // Chapter 2: Variables in String
 // A variable in string means it is a container that stores a value, such as a text string and the text must always be enclosed in quotes.
---
+++ Front-End-Development/Javascript/Javascript-by-SMIT/Practise/4-manipulating-strings/script.js
@@ -0,0 +1,8 @@
+// Chapter 8: Concatenating text strings
+// Concatenation (+): The + operator in JavaScript combines two or more strings into one continuous string.
+// Performing Mathematical operations except addition on strings that contains numeric values, JavaScript automatically converts those strings to numbers before performing the operation.
+var firstName =prompt("First Name: ");
+var lastName =prompt("Last Name: ");
+var fullName = firstName + " " + lastName;
+
+alert("Assalam u Alaikum " + fullName + "\nNice to Meet you!")
