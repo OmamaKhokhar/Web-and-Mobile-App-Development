8d7179b59ba3c68d1afcb23a80e4b1d3e5c231b9
Omama Khokhar <workwithomama@outlook.com>
2024-09-01 02:13:21 +0500

    Learned Chapter 10 - if statements (SMIT)

---
+++ Front-End-Development/Javascript/Javascript-by-SMIT/Practise/5-conditional-statement/script.js
@@ -0,0 +1,13 @@
+// Control Statements: Conditional Statment 
+// A conditional statement is a programming construct that executes different code blocks based on whether a specified condition is true or false.
+
+// Chapter 10: if statements
+// If Statement: A control structure that allows you to execute a block of code only if a specified condition is true.
+// Condition: An expression that evaluates to true or false, determining whether the code block inside the if statement should run.
+// Syntax: An if statement starts with the keyword if, followed by a condition in parentheses that checks whether an expression is true or false, executing the code block inside the curly brackets {} if the condition is true.
+
+var capitalOfPakistan = prompt("Enter the Capital of Pakitstan: ");
+
+if (capitalOfPakistan === "Islamabad"){
+    alert("Correct");
+}
