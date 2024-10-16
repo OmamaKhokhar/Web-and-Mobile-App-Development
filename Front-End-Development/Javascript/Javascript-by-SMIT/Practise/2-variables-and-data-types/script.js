d0619265352922d70f2bb278ae12360ac1447d9d
Omama Khokhar <workwithomama@outlook.com>
2024-10-16 22:09:15 +0500

    Learned Chapter 28 to 29 - String Conversion (SMIT)

--- Front-End-Development/Javascript/Javascript-by-SMIT/Practise/2-variables-and-data-types/script.js
+++ Front-End-Development/Javascript/Javascript-by-SMIT/Practise/2-variables-and-data-types/script.js
@@ -32,3 +32,37 @@ alert("Your Gain Weight is " + gainWeight);
 
 var age = 17;
 alert("Your age is " + age);
+
+// Chapter 28: Converting Strings to Integers and Decimals
+// When a user enters data using prompt, it is treated as a string. Even if the input looks like a number, JavaScript sees it as a string.
+//  JavaScript can automatically convert strings to numbers in math expressions, but not in string concatenation with +.
+
+// Auto-conversion of strings in math expressions
+var num = prompt("Enter the Number :");
+console.log(num - 10);
+
+// NaN occurs when non-numeric strings are used in math
+console.log(num - "cake");
+
+// Use parseInt() to convert strings to integers, and parseFloat() to preserve decimal values.
+var intNumber = parseInt("10.7890");
+var floatNumber = parseFloat("10.7890");
+
+console.log(`ParseInt: ${intNumber}
+PareFloat: ${floatNumber}`);
+
+// Chapter 29: Converting Strings to Numbers, Numbers to Strings
+
+// Converting Strings to Numbers
+// Number() converts both integers and floating-point numbers from strings to numbers.
+var intString = "124";
+console.log(Number(intString));
+
+var intFloat = "124.099756";
+console.log(Number(intFloat));
+
+// Converting Numbers to Strings
+// Use toString() to convert a number into a string for formatting or other purposes.
+var userAge = 18;
+var userAgeString = userAge.toString();
+console.log(userAgeString);
