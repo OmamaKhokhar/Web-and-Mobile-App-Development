f1d122955f93eb279352ee3772de96c8a39e409e
Omama Khokhar <workwithomama@outlook.com>
2024-10-21 00:10:31 +0500

    Learned Chapter 43 - Placing Scripts (SMIT)

--- Front-End-Development/Javascript/Javascript-by-SMIT/Practise/1-introduction/script.js
+++ Front-End-Development/Javascript/Javascript-by-SMIT/Practise/1-introduction/script.js
@@ -14,3 +14,12 @@ var fullName = prompt("Full Name: ");
 var country = prompt("Country: ");
 
 alert("Hi! " + fullName + " from " + country + "\nNice to meet You! \nHave a Nice Day!");
+
+// Chapter 43: Placing Scripts
+// JavaScript can be placed in various parts of an HTML file:
+// 1. Inside the <head> section.
+// 2. Inside the <body> section (beginning, middle, or end).
+
+// Best Practice: Place scripts at the end of the <body> to ensure that the page content loads first
+// External JavaScript files are recommended for better organization and reusability. 
+// Link them using the <script src="file.js"></script> tag, usually placed at the end of the <body>.
